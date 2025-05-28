aa879461f941c70fbe23e83d4b3700819749b3a4
warghane-monali <monaliwarghane97@gmail.com>
2025-05-28 11:19:41 +0530

    Managed ProgessBar buh

--- src/components/HealthStatusCards.tsx
+++ src/components/HealthStatusCards.tsx
@@ -9,13 +9,8 @@ return (
         healthStatusData && healthStatusData?.map((itm, index)=>{
           const {image, bodyPart, date, range} = itm;
 
-            let progressColor = '';
-            if (range <= 25) progressColor = 'bg-red-500';
-            else if (range <= 50) progressColor = 'bg-orange-400';
-            else if (range <= 75) progressColor = 'bg-green-800';
-            else progressColor = 'bg-red-600';
-
-          return(
+       
+       return(
             <div key={index} className="bg-background rounded-3xl p-4 w-full max-w-xs">
               <div className='flex items-center gap-4'>
                 <img src={image} alt={bodyPart} className="w-8"/>
